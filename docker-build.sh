cba699d1a6a9f4bb23f76841ec189a4ccdf2a36c
陈小白→_→。 <522858454@qq.com>
2019-05-18 17:58:57 +0800

    Update docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-docker login -u="${USERNAME}" -p="${TOKEN}" hub.docker.com
+docker login -u="${USERNAME}" -p="${TOKEN}"
 docker build -t chenzhijun/kubernetes-tar:latest .
 docker push chenzhijun/kubernetes-tar:latest
-
-exit 0
